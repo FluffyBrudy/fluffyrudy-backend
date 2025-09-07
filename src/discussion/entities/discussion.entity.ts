553ec7f5ab0b1d225588c843b08759fcd6a67a15
rudy <fluffyrudy@proton.me>
2025-09-07 17:49:27 +0545

    Add Discussion entity with database migration and clean up controller/service

--- src/discussion/entities/discussion.entity.ts
+++ src/discussion/entities/discussion.entity.ts
@@ -1,1 +1,24 @@
-export class Discussion {}
+import { User } from 'src/user/entities/user.entity';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+
+@Entity('discussion', { schema: 'markdown' })
+export class Discussion {
+  @PrimaryGeneratedColumn({ type: 'integer', name: 'id' })
+  id: number;
+
+  @Column('text', { name: 'content' })
+  content: string;
+
+  @Column('character varying', { name: 'post_slug', length: 255 })
+  post_slug: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'user_id', referencedColumnName: 'id' })
+  userId: number;
+}
