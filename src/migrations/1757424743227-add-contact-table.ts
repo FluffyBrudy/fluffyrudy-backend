5c7896cf9e20bb066a7e84198ab64a445ab03d1b
rudy <fluffyrudy@proton.me>
2025-09-11 09:57:16 +0545

    refactor code block

--- src/migrations/1757424743227-add-contact-table.ts
+++ src/migrations/1757424743227-add-contact-table.ts
@@ -1,14 +1,15 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class AddContactTable1757424743227 implements MigrationInterface {
-    name = 'AddContactTable1757424743227'
+  name = 'AddContactTable1757424743227';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "contact" ("id" SERIAL NOT NULL, "username" character varying(255) NOT NULL, "email" character varying(50) NOT NULL, "subject" character varying(255) NOT NULL, "message" character varying(1000) NOT NULL, "created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(), CONSTRAINT "PK_2cbbe00f59ab6b3bb5b8d19f989" PRIMARY KEY ("id"))`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "contact" ("id" SERIAL NOT NULL, "username" character varying(255) NOT NULL, "email" character varying(50) NOT NULL, "subject" character varying(255) NOT NULL, "message" character varying(1000) NOT NULL, "created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(), CONSTRAINT "PK_2cbbe00f59ab6b3bb5b8d19f989" PRIMARY KEY ("id"))`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP TABLE "contact"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "contact"`);
+  }
 }
