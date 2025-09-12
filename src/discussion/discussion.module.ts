bb5732939be9d04c28a7dfaecca9d940f9f53b30
rudy <fluffyrudy@proton.me>
2025-09-12 16:42:08 +0545

    uncomment controller and service in discussion module

--- src/discussion/discussion.module.ts
+++ src/discussion/discussion.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
-// import { DiscussionService } from './discussion.service';
-// import { DiscussionController } from './discussion.controller';
+import { DiscussionService } from './discussion.service';
+import { DiscussionController } from './discussion.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Discussion } from './entities/discussion.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Discussion])],
-  // controllers: [DiscussionController],
-  // providers: [DiscussionService],
+  controllers: [DiscussionController],
+  providers: [DiscussionService],
 })
 export class DiscussionModule {}
